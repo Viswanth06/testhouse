4914bc6c0716fede52c74e45e6c69b5a6135b6d2
Viswanth06 <138104486+Viswanth06@users.noreply.github.com>
2025-01-18 13:42:37 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,72 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sous-sol---music-room",
+      "name": "Sous Sol - Music room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.5682008155160396,
+        "pitch": 0.2570542294309295,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.555745691379152,
+          "pitch": 0.019985098512059096,
+          "rotation": 0,
+          "target": "1-sous-sol---game-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sous-sol---game-room",
+      "name": "Sous Sol - game room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
